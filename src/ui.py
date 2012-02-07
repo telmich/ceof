c2c086fdd173d761c84ec9477f7e1a6ae1d61cc6
Nico Schottelius <nico@brief.schottelius.org>
2012-02-07 20:55:04 +0100

    draw a border
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,30 @@
 import curses
+import time
 
 stdscr = curses.initscr()
 
+# Do not display stuff pressed
+curses.noecho()
+
+# React on characters without return
+curses.cbreak()
+
+# Enable cursor support
+stdscr.keypad(1)
+
+begin_x = 20
+begin_y = 7
+height = 5
+width = 40
+
+#win = curses.newwin(height, width, begin_y, begin_x)
+stdscr.border(0)
+stdscr.refresh()
+
+time.sleep(2)
+
+
+# Exit
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
