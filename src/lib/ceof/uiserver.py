0c2036a7fff02b89e5b317e9c31005d4d749fe91
Nico Schottelius <nico@brief.schottelius.org>
2012-03-20 12:22:47 +0100

    add uiserver template
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

---
+++ src/lib/ceof/uiserver.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# 2012 Nico Schottelius (nico-ceof at schottelius.org)
+#
+# This file is part of ceof.
+#
+# ceof is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ceof is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ceof. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import ceof
+import logging
+import os
+
+log = logging.getLogger(__name__)
+
+class ServerError(ceof.Error):
+    pass
+
+class UIServer(object):
+    """Setup the UI server to accept connections"""
+
+    def __init__(self, address, port):
+        self._init_config_dir(config_dir)
+        self._verify_config_dir()
+        self._create_directories()
+
+    def _init_config_dir(self, config_dir):
+        """Find/setup configuration config_dir"""
+        # Prefer what the user said
+        if config_dir:
+            self.config_dir = config_dir
+        else:
+            if 'HOME' in os.environ:
+                self.config_dir = os.path.join(os.environ['HOME'], '.ceof')
+            else:
+                self.config_dir = None
+
+    def _verify_config_dir(self):
+        """Verify configuration directory option"""
+        if not self.config_dir:
+            raise ConfigError("Cannot find configuration directory")
+
+    def _create_directories(self):
+        """Create required configuration directories"""
+        os.makedirs(self.config_dir, exist_ok=True)
+        os.makedirs(self.gpg_config_dir, exist_ok=True)
+
+    @property
+    def gpg_config_dir(self):
+        return os.path.join(self.config_dir, "gnupg")
