e1ef5bce4724e28cebf44224d9a113c292345216
Nico Schottelius <nico@brief.schottelius.org>
2012-04-17 20:42:26 +0200

    add template for sender server
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

---
+++ src/lib/ceof/server/sender.py
@@ -0,0 +1,120 @@
+# 2012 Nico Schottelius (nico-ceof at schottelius.org)
+#
+# This file is part of ceof.
+#
+# ceof is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ceof is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ceof. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+
+import ceof
+import logging
+import select
+import queue
+import socket
+import multiprocessing
+import urllib.parse
+import time
+
+log = logging.getLogger(__name__)
+
+#class ServerError(ceof.Error):
+#    pass
+
+class Listener(object):
+    """Listen server"""
+
+    def __init__(self, listener, queue=None):
+        self.listener = listener
+
+        # Queue to put received packets into
+        self.queue      = {}
+        self.process    = {}
+        self.fds        = []
+
+        self.upstream_queue = queue
+
+    # Start own process for each listener => use available cores!
+    def run(self):
+        for listener in self.listener:
+            url = urllib.parse.urlparse(listener)
+
+            address, port = url.netloc.split(":")
+            name = address + port
+
+            print("Listening on %s:%s" % (address, port))
+            print(self.queue)
+            print(type(self.queue))
+
+            self.queue[name]  = multiprocessing.Queue()
+            self.process[name] = multiprocessing.Process(target=self.child, 
+                args=(address, port, self.queue[name],))
+            self.process[name].start()
+
+            # File deskriptors for select()
+            self.fds.append(self.queue[name]._reader)
+
+        while True:
+            # wait for input - unportable it seems
+            #(select_res,[],[]) = select.select(self.fds,[],[])
+
+            for q in self.queue.values():
+                data = False
+                try:
+                    data = q.get(block=False)
+                except queue.Empty:
+                    pass
+
+                if data:
+                    message = data.decode('utf-8')
+                    self.upstream_queue.put(message)
+                    log.debug("Forwarded message: %s to upstream" % (message))
+
+            # Spinner - ugly, but not as ugly as searching for fds
+            # returned by select and match on queue and get then...
+            time.sleep(0.5)
+
+        #p.join()
+
+    def child(self, address, port, queue):
+        print("running in child")
+        self.child_queue = queue
+        server = ceof.server.tcp.TCPServer(address, port, handler=self.child_handler)
+
+        try:
+            server.run()
+        except socket.error as e:
+            print("Failed to run on %s:%s: %s" % (address, port, e))
+
+    def child_handler(self, conn, addr):
+        print("Connected by %s" % str(addr))
+
+        data = []
+        while 1:
+            try:
+                tmp = conn.recv(1024)
+                if not tmp:
+                    break
+            
+                data.append(tmp)
+
+            except (socket.error, KeyboardInterrupt):
+                conn.close()
+                raise
+
+        # Done, send data
+        message = b''.join(data)
+        print("Submitting data to parent: %s" % message.decode('utf-8'))
+        self.child_queue.put(message)
+        conn.close()
